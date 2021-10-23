c809de286101338bd6d40abf585567934374bda6
Polina Shneider <beatrixlinkoln@gmail.com>
2021-10-24 01:11:30 +0300

    Integration attempt

--- frontend/src/components/form/Form.tsx
+++ frontend/src/components/form/Form.tsx
@@ -9,8 +9,8 @@ import Recommendations from "../recommendations/Recommendations";
 const MAX_PARAMETERS = 5;
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
-const PARAMS_URL = `${SERVER_URL}/params`;
-const USER_CHOICE = `${SERVER_URL}/save`;
+const PARAMS_URL = `${SERVER_URL}/Parameters`;
+const USER_CHOICE = `${SERVER_URL}/Recommendation`;
 
 type HandleInputProps = {
     value: any;
@@ -18,18 +18,10 @@ type HandleInputProps = {
     name: string;
 };
 
-const initialParams = {
-    'department': [],
-    'stage': [],
-    'market': [],
-    'technologies': [],
-    'legal': []
-};
-
 const Basic = () => {
     const {handleSubmit, control, getValues, setError, clearErrors, formState: {errors}} = useForm();
-    const [modelParams, setParams] = useState(initialParams);
-    const [recommendations, setRecommendations] = useState([]);
+    const [modelParams, setParams] = useState<ParameterDefinition[] | undefined>([]);
+    const [recommendations, setRecommendations] = useState<RecommendationInfo[] | undefined>([]);
 
     const onSubmit = () => {
         sendUserChoice(getValues());
@@ -37,18 +29,16 @@ const Basic = () => {
     };
 
     const sendUserChoice = (values: UnpackNestedValue<FieldValues>) => {
-        axios.post(USER_CHOICE, {
+        axios.post<GetRecommendationResponse>(USER_CHOICE, {
             body: values,
-        }).then(res => {
-            const {data}: any = res;
+        }).then(({data}) => {
             setRecommendations(data.recommendations);
         })
     };
 
     const fetchParams = () => {
-        axios.get(PARAMS_URL).then(res => {
-            const {data}: any = res;
-            setParams(data.params);
+        axios.get<GetParametersResponse>(PARAMS_URL).then(({data}) => {
+            setParams(data.parameters);
         })
     };
 
@@ -72,94 +62,27 @@ const Basic = () => {
     return (
         <div>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                    <label htmlFor="legal">Юр.лицо</label>
-                    <Controller
-                        render={
-                            ({field}) => <Select
-                                {...field}
-                                placeholder="Юр. лицо"
-                                options={modelParams.legal}
+                {(modelParams || []).map((param: ParameterDefinition) => {
+                    return (
+                        <div>
+                            <label htmlFor="legal">{param.id}</label>
+                            <Controller
+                                render={
+                                    ({field}) => <Select
+                                        {...field}
+                                        placeholder={param.id}
+                                        isMulti={param.type === "MultiAcceptable"}
+                                        options={param.acceptableValues}
+                                    />
+                                }
+                                control={control}
+                                name={param.id ?? ""}
+                                defaultValue={param.type === "MultiAcceptable" ? [] : ""}
                             />
-                        }
-                        control={control}
-                        name="legal"
-                        defaultValue={""}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="technologies">Технологии</label>
-                    <Controller
-                        render={
-                            ({field}) => <Select
-                                {...field}
-                                placeholder="Ваши технологии"
-                                onChange={(value) => handleChange({
-                                    value, onChange: field.onChange, name: field.name
-                                })}
-                                isMulti
-                                options={modelParams.technologies}
-                            />
-                        }
-                        control={control}
-                        name="technologies"
-                        defaultValue={[]}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="market">Рынок</label>
-                    <Controller
-                        render={
-                            ({field}) => <Select
-                                {...field}
-                                placeholder="Рынок"
-                                onChange={(value) => handleChange({
-                                    value, onChange: field.onChange, name: field.name
-                                })}
-                                isMulti
-                                options={modelParams.market}
-                            />
-                        }
-                        control={control}
-                        name="market"
-                        defaultValue={[]}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="stage">Стадия развития</label>
-                    <Controller
-                        render={
-                            ({field}) => <Select
-                                {...field}
-                                placeholder="Стадия развития"
-                                options={modelParams.stage}
-                            />
-                        }
-                        control={control}
-                        name="stage"
-                        defaultValue={""}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="department">Технологическая ниша</label>
-                    <Controller
-                        render={
-                            ({field}) => <Select
-                                {...field}
-                                placeholder="Технологическая ниша"
-                                onChange={(value) => handleChange({
-                                    value, onChange: field.onChange, name: field.name
-                                })}
-                                isMulti
-                                options={modelParams.department}
-                            />
-                        }
-                        control={control}
-                        name="department"
-                        defaultValue={[]}
-                    />
-                    {errors.department && <p>{errors.department.message}</p>}
-                </div>
+                            {errors[param.id ?? ""] && <p>{errors[param.id ?? ""].message}</p>}
+                        </div>
+                    )
+                })}
                 <input type="submit" value="Отправить данные"/>
             </form>
             <Recommendations items={recommendations}/>
--- frontend/src/components/recommendations/Recommendations.tsx
+++ frontend/src/components/recommendations/Recommendations.tsx
@@ -1,20 +1,14 @@
 import React from "react";
 import "./styles.css";
 
-export interface Recommendation {
-    id: string;
-    title: string;
-    score: number;
-}
-
-const RecommendationItem = (props: {item: Recommendation, index: number}) => {
+const RecommendationItem = (props: {item: RecommendationInfo, index: number}) => {
     const {item, index} = props;
     const width = (100 - index * 10) > 10 ? (100 - index * 10) : 10;
     return (
         <div className="recommendation-item">
             <div>
-                <span className="recommendation-item__score">{item.score}%</span>
-                <span className="recommendation-item__title">{item.title}</span>
+                <span className="recommendation-item__score">{item.matchPercentage}%</span>
+                <span className="recommendation-item__title">{item.id}</span>
             </div>
             <div style={{
                 width: `${width}%`
@@ -24,13 +18,13 @@ const RecommendationItem = (props: {item: Recommendation, index: number}) => {
 };
 
 export type RecommendationProps = {
-    items: Recommendation[];
+    items?: RecommendationInfo[];
 }
 
 const List = (props: RecommendationProps) => {
     const {items} = props;
 
-    if (!items.length) {
+    if (!items || !items.length) {
         return null;
     }
     return (
